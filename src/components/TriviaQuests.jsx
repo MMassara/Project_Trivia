a8eebf31fe647b54bc6bb62318093b2b0e71b823
Matheus Massara <matheus.massara@hotmail.com>
2022-10-11 16:55:47 -0300

    Req 7 sem lint

--- src/components/TriviaQuests.jsx
+++ src/components/TriviaQuests.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
+
 class TriviaQuests extends Component {
   state = {
     arrayResults: [],
     arrayIndex: 0,
-    answers: '',
+    answers: [],
     invalidToken: false,
+    showResults: false,
   };
 
   componentDidMount() {
     this.fetchApi();
+  }
+
+  createAnswers = () => {
+    const {arrayResults, arrayIndex} = this.state;
+    const results = arrayResults[arrayIndex].incorrect_answers.map((element, index) => {
+      const incorrectAnswers = {
+        name: element,
+        testid: `wrong-answer-${index}`,
+        isRight: false
+      };
+      return incorrectAnswers;
+    });
+    const correct = {
+      name: arrayResults[arrayIndex].correct_answer,
+      testid: 'correct-answer',
+    };
+    const answerss = [...results, correct];
+    const MULTIPLE = 0.5;
+    const shuffle = answerss.sort(() => Math.random() - MULTIPLE);
+    this.setState({
+      answers: shuffle,
+    });
   }
 
   fetchApi = async () => {
@@ -20,44 +44,32 @@ class TriviaQuests extends Component {
     );
     const response = await request.json();
     const magicNumber = 3;
-    console.log(response);
     if (!response.results.length && response.response_code === magicNumber) {
       localStorage.setItem('token', '');
       this.setState({
         invalidToken: true,
       });
-    } else {
-      const results = response.results[0].incorrect_answers.map((element, index) => {
-        const incorrectAnswers = {
-          name: element,
-          testid: `wrong-answer-${index}`,
-        };
-        return incorrectAnswers;
-      });
-      const correct = {
-        name: response.results[0].correct_answer,
-        testid: 'correct-answer',
-      };
-      const answerss = [...results, correct];
-      const MULTIPLE = 0.5;
-      const shuffle = answerss.sort(() => Math.random() - MULTIPLE);
-      this.setState({
-        answers: shuffle,
-      });
-    }
+    } 
     this.setState({
-      arrayResults: response.results,
-    });
+      arrayResults: response.results, 
+    }, () => this.createAnswers());
   };
 
   handleClick = () => {
     this.setState((prevState) => ({
       arrayIndex: prevState.arrayIndex + 1,
-    }));
+      showResults:false
+    }), () => this.createAnswers());
   };
 
+  showResponses = () => {
+    this.setState({
+      showResults: true,
+    })
+  }
+
   render() {
-    const { arrayResults, arrayIndex, answers, invalidToken } = this.state;
+    const { arrayResults, arrayIndex, answers, invalidToken, showResults } = this.state;
 
     return (
       <div>
@@ -81,6 +93,8 @@ class TriviaQuests extends Component {
                         key={ question.name }
                         type="button"
                         data-testid={ question.testid }
+                        onClick={this.showResponses}
+                        className={question.testid === 'correct-answer' && showResults === true ? 'green-border' : null || question.isRight === false && showResults === true ? 'red-border' : null }
                       >
                         {question.name}
                       </button>
@@ -90,6 +104,8 @@ class TriviaQuests extends Component {
                         key={ question.name }
                         type="button"
                         data-testid={ question.testid }
+                        onClick={this.showResponses}
+                        className={question.testid === 'correct-answer' && showResults === true ? 'green-border' : null || question.isRight === false && showResults === true ? 'red-border' : null }
                       >
                         {question.name}
                       </button>
